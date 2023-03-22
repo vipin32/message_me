364166f9535c3f168ac45d304f5597575ce376ee
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-03-22 08:34:02 +0530

    fixed modified login form and handled user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:user][:email])
 
         if user && user.authenticate(params[:user][:password])
-            # User is authenticated
+            flash[:success] = "Logged in successfully!";
+            redirect_to root_path
         else
-            # Invalid email or password
+            flash[:danger] = "Email or Password Wrong!";
+            redirect_to login_path
         end
     end
     
