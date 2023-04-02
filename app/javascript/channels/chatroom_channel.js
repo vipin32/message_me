6f00e9b57a9a38d4ea0ab15d7995c280815dc094
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-04-02 15:02:30 +0530

    added js for displaying message\s

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,6 +11,10 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    alert(data.foo)
+
+    // alert(data.foo)
+
+    $("#message_box").append("<div class='outgoing_msg'><div class='sent_msg'><p>"+ data.foo +", Test</p><span class='time_date'> 11:01 AM    |    Today</span></div></div>");
+    // $("#message_box").text('dfghjkvbnm');
   }
 });
